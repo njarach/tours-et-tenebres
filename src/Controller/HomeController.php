aa8231f2d0ed5564a77b4e48d9c58055b335d1d0
njarach <101182825+njarach@users.noreply.github.com>
2024-05-31 11:58:25 +0200

    + ADDED sidebar layout starter

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
@@ -23,7 +24,7 @@ class HomeController extends AbstractController
      * @throws Exception
      */
     #[Route('/markdown/{filePath}', name: 'markdown_render')]
-    public function renderMarkdownFile(string $filePath): Response
+    public function renderMarkdownFile(string $filePath, SessionInterface $session): Response
     {
         if (!file_exists($filePath)) {
             throw $this->createNotFoundException('The markdown file does not exist');
@@ -31,20 +32,42 @@ class HomeController extends AbstractController
 
         $parsedContent = $this->markdownService->parseMarkdownFile($filePath);
 
-        return $this->render('markdown/render.html.twig', [
-            'content' => $parsedContent['content'],
-            'metadata' => $parsedContent['metadata']
-        ]);
-    }
+        // Get the previous file path from the session
+        $previousFilePath = $session->get('previous_file_path');
 
-    #[Route('/markdown_list',name: 'markdown_list')]
-    public function listMarkdownFiles(): Response
-    {
+        // Store the current file path in the session
+        $session->set('previous_file_path', $filePath);
+
+        // Extract the previous file name from the path
+        $previousFileName = null;
+        if ($previousFilePath) {
+            $previousFileName = pathinfo($previousFilePath, PATHINFO_FILENAME);
+        }
+
+        // Store the current file path in the session
+        $session->set('previous_file_path', $filePath);
+
+        // List of markdown files and directories
         $markdownDirectory = $this->getParameter('kernel.project_dir') . '/markdown_files/';
         $directoryTree = $this->scanDirectory($markdownDirectory);
 
-        return $this->render('markdown/list.html.twig', [
-            'directoryTree' => $directoryTree,
+        return $this->render('markdown/render.html.twig', [
+            'content' => $parsedContent['content'],
+            'metadata' => $parsedContent['metadata'],
+            'previous_file_path' => $previousFilePath, // Pass the previous file path to the template
+            'previous_file_name' => $previousFileName, // Pass the previous file name to the template
+            'directoryTree' => $directoryTree
+        ]);
+    }
+
+    #[Route('/home',name: 'home')]
+    public function listMarkdownFiles(): Response
+    {
+        // List of markdown files and directories
+        $markdownDirectory = $this->getParameter('kernel.project_dir') . '/markdown_files/';
+        $directoryTree = $this->scanDirectory($markdownDirectory);
+        return $this->render('home/home.html.twig', [
+            'directoryTree' => $directoryTree
         ]);
     }
 
